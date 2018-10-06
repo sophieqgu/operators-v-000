0da599afd2bc6b5d2adbadc79607a09336e58556
Sophie Gu <sophieqgu@gmail.com>
2018-10-06 07:08:38 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if speed > 60 && speed < 40
+  if speed > 60 || speed < 40
     true 
   else 
     false 
@@ -9,7 +9,7 @@ end
 
 
 def not_safe?(speed)
-	speed > 60 && speed < 40 ? true : false 
+	speed > 60 || speed < 40 ? true : false 
 end
 	
 
